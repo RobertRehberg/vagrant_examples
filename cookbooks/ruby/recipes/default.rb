0b85d1abe0b60464cdaf01f89005177075c8f7e6
Robert Rehberg <robertrehberg@bellsouth.net>
2012-11-21 16:23:32 -0500

    Last bit of script cleanup

--- cookbooks/ruby/recipes/default.rb
+++ cookbooks/ruby/recipes/default.rb
@@ -1,6 +1,8 @@
 ruby_version = '1.9.2-p320'
+source_dir = '/tmp/src/ruby'
+install_dir = '/opt/ruby'
 
-if File.exists?('/opt/ruby')
+if File.exists?(install_dir)
   log 'Ruby already installed'
 else
   packages_to_install = ['unzip', 'build-essential', 'libreadline-dev', 'libssl-dev', 'libyaml-dev', 'libffi-dev',
@@ -11,74 +13,63 @@ else
     end
   end
 
-  directory '/tmp/src/ruby' do
+  directory source_dir do
     recursive true
     action :delete
   end
 
-  directory '/tmp/src/ruby' do
+  directory source_dir do
     recursive true
     mode 00777
     action :create
   end
 
   execute 'unzip sources' do
-    command "unzip /vendor/ruby/#{ruby_version}/ruby_ubuntu.zip -d /tmp/src/ruby"
+    command "unzip /vendor/ruby/#{ruby_version}/ruby_ubuntu.zip -d #{source_dir}"
   end
 
-  file '/tmp/src/ruby/configure' do
+  file "#{source_dir}/configure" do
     mode 00777
   end
 
-  file '/tmp/src/ruby/tool/ifchange' do
+  file "#{source_dir}/tool/ifchange" do
     mode 00777
   end
 
   execute 'configure' do
-    command "./configure --enable-shared --prefix=/opt/ruby/#{ruby_version}"
-    cwd '/tmp/src/ruby'
+    command "./configure --enable-shared --prefix=#{install_dir}/#{ruby_version}"
+    cwd source_dir
   end
 
-  execute 'make all' do
-    command "make all"
-    cwd '/tmp/src/ruby'
+  make_commands = ['all', 'test', 'install']
+  make_commands.each do |cmd|
+    execute "make #{cmd}" do
+      command "make #{cmd}"
+      cwd source_dir
+    end
   end
 
-  execute 'make test' do
-    command "make test"
-    cwd '/tmp/src/ruby'
-  end
-
-  execute 'make install' do
-    command "make install"
-    cwd '/tmp/src/ruby'
-  end
-
-  link '/opt/ruby/active' do
+  link "#{install_dir}/active" do
     action :delete
-    only_if 'test -L /opt/ruby/active'
-  end
-
-  file '/usr/bin/ruby' do
-    action :delete
-  end
-
-  file '/usr/bin/gem' do
-    action :delete
-  end
-
-  file '/usr/bin/rake' do
-    action :delete
+    only_if "test -L #{install_dir}/active"
   end
 
   execute 'create sym link' do
     command "ln -fs #{ruby_version} active"
-    cwd '/opt/ruby'
+    cwd install_dir
   end
 
-  ['ruby', 'gem', 'rake', 'rspec', 'rdoc', 'ri', 'bundle'].each do |exe|
+  executables = ['ruby', 'gem', 'rake', 'rspec', 'rdoc', 'ri', 'bundle']
+
+  executables.each do |exe|
+    file "/usr/bin/#{exe}" do
+      action :delete
+    end
+  end
+
+  executables.each do |exe|
     link "/usr/bin/#{exe}" do
-      to "/opt/ruby/active/bin/#{exe}"
+      to "#{install_dir}/active/bin/#{exe}"
     end
   end
 end
